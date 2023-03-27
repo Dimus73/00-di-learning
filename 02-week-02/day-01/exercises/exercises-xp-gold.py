4d23dbb0e9b17b2ad2aa153ac5e054fee72acb3d
Dimus73 <dmitry.prigozhin@gmail.com>
2023-03-27 07:46:31 +0300

    1111

--- 02-week-02/day-01/exercises/exercises-xp-gold.py
+++ 02-week-02/day-01/exercises/exercises-xp-gold.py
@@ -1,1 +1,17 @@
+print ("Exercise 1:")
 print ("Hello world\n"*5, "I love python\n"*5)
+
+print("\nExercise 2:")
+month=0
+while not(month>=1 and month<=12):
+		month_str=input("Enter the month (1-12): ")
+		month = int(month_str)
+if month >= 3 and month <=5:
+    season="Spring"
+elif month >= 6 and month <=8:
+    season="Summer"
+elif month >= 9 and month <=11:
+    season="Autumn"
+else: season="Winter"
+
+print (f"The season is {season}")
---
+++ 02-week-02/day-01/exercises/exercises-xp-ninja.py
@@ -0,0 +1,7 @@
+my_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+print (len(my_text))
+
+streeng_text=input("Input the longest sentence they can without the character “A”")
+if streeng_text.find("A") == -1 and streeng_text.find("a") == -1:
+    print ("Congratulations!")
+else: print("error")
