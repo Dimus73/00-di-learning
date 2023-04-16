e48aa411a6e35182cf06dfeee22471cd015d9a21
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-16 14:45:53 +0300

    exercise-xp-w04-d01

---
+++ 04-week-04/day-01/exercises/exercise-xp-w04-d01.sql
@@ -0,0 +1,50 @@
+-- CREATE table items (
+-- 	id serial primary key,
+-- 	item_name varchar(20),
+-- 	price float
+-- );
+
+-- CREATE table customers (
+-- 	id serial primary key,
+-- 	FierstName varchar(10),
+-- 	LastName varchar(10)
+-- );
+
+-- insert INTO items (item_name,price)
+-- VALUES ('Small Desk', 100),
+-- ('Large',300),
+-- ('Fan',80);
+
+-- INSERT INTO customers (FierstName, LastName)
+-- VALUES ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson')
+
+-- select item_name, price
+-- from items;
+
+-- select *
+-- from customers;
+
+-- select item_name
+-- from items
+-- where price>80;
+
+-- select item_name
+-- from items
+-- where price<=300;
+
+-- select FierstName, LastName
+-- from customers
+-- where LastName='Smith';
+
+-- select FierstName, LastName
+-- from customers
+-- where LastName='Jones';
+
+-- select FierstName, LastName
+-- from customers
+-- where FierstName='Scott';
+
