78abe9ee8d92160e38073bb654f1fb9d4f683e69
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-16 18:57:13 +0300

    daily-challenge-w04-d01

---
+++ 04-week-04/day-01/exercises/daily-challenge-w04-d01.sql
@@ -0,0 +1,20 @@
+-- create table actors (
+-- 	id serial primary key,
+-- 	first_name varchar(20), 
+-- 	last_name varchar(20), 
+-- 	age date, 
+-- 	number_oscars int
+-- );
+ 
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt1','Damon1','1971-10-07', 5);
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George1','Clooney1','1981-09-06', 2);
+
+-- select count(id)
+-- from actors
+
+-- INSERT INTO actors (first_name)
+-- VALUES('George2');
+
+select * from actors
