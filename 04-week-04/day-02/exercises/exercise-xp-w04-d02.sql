0d47c19e5bc069388e9ae8219dec6ac6352200ce
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-17 13:24:57 +0300

    exercise-xp-w04-d02

---
+++ 04-week-04/day-02/exercises/exercise-xp-w04-d02.sql
@@ -0,0 +1,18 @@
+
+select * 
+from items
+order by  price desc;
+
+select * 
+from items
+order by  price;
+
+select *
+from items
+where price >= 80
+order by price desc;
+
+select fierstname, lastname
+from customers
+order by fierstname
+LIMIT 3
