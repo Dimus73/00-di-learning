7f04b4100901f4308dada810665ee4add085c98c
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-20 00:21:38 +0300

    exercises-gold-w04-d04

---
+++ 04-week-04/day-04/exercises/exercises-gold-w04-d04.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+HOSTNAME = 'localhost'
+USERNAME = 'dmitryprigozhin'  # postgres
+PASSWORD = ''
+DATABASE = 'exer-w04-d04'
+TABLENAME = 'users'
+
+
+def load_data(connection):
+    print(id(connection), TABLENAME)
+    with connection.cursor() as cur:
+        cur.execute(f'SELECT login,pw FROM {TABLENAME}')
+        result=cur.fetchall()
+    for p in result:
+        users=dict(result)
+    return users
+
+def add_user_to_base(connection, login, pw):
+    with connection.cursor() as cur:
+        cur.execute(f"INSERT INTO {TABLENAME} (login, pw) VALUES ('{login}','{pw}')")
+        connection.commit()
+
+connection=psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
+users=load_data(connection)
+# users={'Login1':'pw001','Login2':'pw002','Login3':'pw003'}
+while True:
+    enter=input("Enter login or exit: ")
+    if enter == 'exit':
+        break
+    elif enter == 'login':
+        login=input('Enter login: ')
+        pw=input('Enter passwod: ')
+        if login in users.keys():
+            if users[login] == pw:
+                logged_in=login
+        else:
+            like_singup=input("Would you like to register? (y/n): ")
+            if like_singup == 'y':
+                pw=input("Please enter your new password: ")
+                add_user_to_base(connection,login,pw)
+
+    
