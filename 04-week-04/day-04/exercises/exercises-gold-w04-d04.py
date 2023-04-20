f24d466623dcab512c397ca92af354e901a39559
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-20 08:13:17 +0300

    exercises-gold-w04-d04

--- 04-week-04/day-04/exercises/exercises-gold-w04-d04.py
+++ 04-week-04/day-04/exercises/exercises-gold-w04-d04.py
@@ -8,21 +8,23 @@ TABLENAME = 'users'
 
 
 def load_data(connection):
-    print(id(connection), TABLENAME)
     with connection.cursor() as cur:
         cur.execute(f'SELECT login,pw FROM {TABLENAME}')
         result=cur.fetchall()
-    # for p in result:
-    #     users=dict(result)
-    return dict(result)
+    decrypt=dict(result)
+    for k,p in decrypt.items():
+         decrypt[k]="".join([chr(ord(a)-1) for a in p])
+    return decrypt
 
 def add_user_to_base(connection, login, pw):
+    encrypt_pw="".join([chr(ord(a)+1) for a in pw])
     with connection.cursor() as cur:
-        cur.execute(f"INSERT INTO {TABLENAME} (login, pw) VALUES ('{login}','{pw}')")
+        cur.execute(f"INSERT INTO {TABLENAME} (login, pw) VALUES ('{login}','{encrypt_pw}')")
         connection.commit()
 
 connection=psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
 users=load_data(connection)
+print (users)
 # users={'Login1':'pw001','Login2':'pw002','Login3':'pw003'}
 while True:
     enter=input("Enter login or exit: ")
