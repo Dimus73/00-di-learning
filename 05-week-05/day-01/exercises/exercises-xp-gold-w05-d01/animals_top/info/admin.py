84942a3cda3028aecdda552282df2dcd4b97ef76
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-22 16:22:34 +0300

    exercises-xp-gold-w05-d01 finish

--- 05-week-05/day-01/exercises/exercises-xp-gold-w05-d01/animals_top/info/admin.py
+++ 05-week-05/day-01/exercises/exercises-xp-gold-w05-d01/animals_top/info/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Families, Animals
+
+admin.site.register(Families)
+admin.site.register(Animals)
