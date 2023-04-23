fd0a24a1857b5c4d445f4ea5736a98a5cbb1fc79
Dimus73 <dmitry.prigozhin@gmail.com>
2023-04-23 05:10:15 +0300

    exercises-xp-ninja-w05-d01

--- 05-week-05/day-01/exercises/exercises-xp-gold-w05-d01/animals_top/info/views.py
+++ 05-week-05/day-01/exercises/exercises-xp-gold-w05-d01/animals_top/info/views.py
@@ -6,25 +6,20 @@ from info.models import *
 
 # Create your views here.
 def family (request, family_id):
-    with open('data.json','r') as f:
-        data=json.load(f)
+    # with open('data.json','r') as f:
+    #     data=json.load(f)
 
-    data_dic={'data':data}
-    context={}
-    context['animals_name']= [f['name'] for f in data_dic['data']['animals'] if int(f['family']) == int(family_id)]
-    context['family_id']=family_id
+    # data_dic={'data':data}
+    # context['animals_name']= [f['name'] for f in data_dic['data']['animals'] if int(f['family']) == int(family_id)]
+    # context['family_id']=family_id
+    context = {}
+    context['fmily'] = Families.objects.get(pk=family_id)
+    context['animals'] = Animals.objects.filter(family_id=family_id)
     print (context, type(family_id))
     
     return render(request, 'info/family.html', context)
 
 def animal (request, animal_id):
-    # with open('data.json','r') as f:
-    #     data=json.load(f)
-
-    # data_dic={'data':data}
-    # context={}
-    # context['animals']=data_dic['data']['animals'][animal_id-1]
-    # context['animal_id']=int(animal_id)
     context=Animals.objects.get(pk=animal_id)
     print(context.all_info_dict())
     return render(request, 'info/animal.html',{'context':context})
