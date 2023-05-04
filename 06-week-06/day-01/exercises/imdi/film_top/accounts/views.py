06d14e1dc3ad7cc8df8f9c4130ff2ae3bc9f6e38
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-04 21:58:22 +0300

    Work on the signal

--- 06-week-06/day-01/exercises/imdi/film_top/accounts/views.py
+++ 06-week-06/day-01/exercises/imdi/film_top/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.shortcuts          import render
+from django.views.generic      import CreateView, UpdateView
+from django.urls               import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from utils                     import *
+from accounts.forms            import *
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'accounts/registry.html'
+    success_url = reverse_lazy('homepage_path')
+
+    def get_context_data(self, *,object_list=None, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title = "Register user")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+class LoginUser(DataMixin,LoginView):
+    form_class=AuthenticationForm
+    template_name = 'accounts/login.html'
+    
+    def get_context_data(self, *,object_list=None, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title = "User log in")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+class Profile(DataMixin,UpdateView):
+    pass
