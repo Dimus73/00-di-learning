82bf787a0ba1afedccd08d98d36af3bcc656e932
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-17 09:40:25 +0300

    v-0001

---
+++ 07-week-07/day-04/in_class/js/script.js
@@ -0,0 +1,33 @@
+console.log("Hellow word!")
+let addressNumber = 5
+let addrStreet = 'Dddddddd'
+let country = 'Israel'
+let tt
+
+console.log ('test', addressNumber, addrStreet, country)
+
+tt = country.concat(" ", "test ", addrStreet, " ", addressNumber)
+
+console.log (tt)
+
+tt = "I live".concat(" ", tt)
+
+console.log (tt)
+
+fff={
+	username:'test',
+	password:'pwpw'
+}
+databse=[fff]
+
+ddd={
+	username:'test',
+	timeline:'tltl'
+}
+
+newsfeed = [ddd,ddd,ddd]
+
+
+console.log(fff)
+console.log(databse)
+console.log(newsfeed)
