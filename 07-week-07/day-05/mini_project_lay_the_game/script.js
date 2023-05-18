4f7b746646e0f227d80147908c4842db5bf4befd
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-18 11:20:53 +0300

    0001

--- 07-week-07/day-05/mini_project_lay_the_game/script.js
+++ 07-week-07/day-05/mini_project_lay_the_game/script.js
@@ -7,38 +7,40 @@ function playTheGame(){
 	let winner = false
 	let computerNumber = Math.round(Math.random()*(10-1)+1);
 
-	while (attempt <= 3 && !winner){
-		do {
-			user_numb = parseInt(prompt(q));
-			if ( isNaN(user_numb) ){
-				q = "You didn't enter a number. Please enter a number from 1 to 10";
-				console.log(q)
-				user_numb = 'z';
-			} else if (user_numb < 1 || user_numb > 10){
-				q = "A number between 1 and 10 is required. Please try again";
-				user_numb = 'z';
+	if (confirm("Do you want to play?")){
+		while (attempt <= 3 && !winner){
+			do {
+				user_numb = parseInt(prompt(q));
+				if ( isNaN(user_numb) ){
+					q = "You didn't enter a number. Please enter a number from 1 to 10";
+					console.log(q)
+					user_numb = 'z';
+				} else if (user_numb < 1 || user_numb > 10){
+					q = "A number between 1 and 10 is required. Please try again";
+					user_numb = 'z';
+				}
+			} while (isNaN(user_numb))
+			console.log(user_numb);
+			console.log(computerNumber);
+			res = compareNumbers (computerNumber,user_numb);
+			switch (res){
+				case 0:
+					winner = true;
+					break;
+				case -1:
+					q ='Your number is more than necessary. Try again'
+					break;
+				case 1:
+					q ='Your number is less than necessary. Try again'
+					break;
 			}
-		} while (isNaN(user_numb))
-		console.log(user_numb);
-		console.log(computerNumber);
-		res = compareNumbers (computerNumber,user_numb);
-		switch (res){
-			case 0:
-				winner = true;
-				break;
-			case -1:
-				q ='Your number is more than necessary. Try again'
-				break;
-			case 1:
-				q ='Your number is less than necessary. Try again'
-				break;
+			attempt++;
 		}
-		attempt++;
-	}
-	if (winner){
-		alert('WINNER')
-	} else {
-		alert ("You didn't guess. Try another time")
+		if (winner){
+			alert('WINNER')
+		} else {
+			alert ("You didn't guess. Try another time")
+		}
 	}
 
 }
