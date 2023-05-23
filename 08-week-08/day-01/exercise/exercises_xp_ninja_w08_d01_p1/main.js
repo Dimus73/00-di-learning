8fb5ae0a78c2a89d174b61cfe5e82295b685145d
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-23 08:45:31 +0300

    exercises_xp_ninja_w08_d01_p1

---
+++ 08-week-08/day-01/exercise/exercises_xp_ninja_w08_d01_p1/main.js
@@ -0,0 +1,29 @@
+function calculateTip(e){
+	elements = e.target.parentElement;
+	console.log(e, elements);
+
+	let billAmount = document.querySelector('#billamt').value;
+	let serviceQuality = document.querySelector('#serviceQual').value;
+	let numberOfPeopleTag = document.querySelector('#peopleamt');
+	let numberOfPeople = Number(numberOfPeopleTag.value);
+	let totalTipTag = document.querySelector('#totalTip')
+	let TipTag = document.querySelector('#tip')
+  console.log(billAmount, typeof serviceQuality, serviceQuality, typeof numberOfPeople, numberOfPeople);
+	if (billAmount === "" || serviceQuality == 0){
+		alert ("Enter bil ammount and service");
+	}
+	each = document.querySelector("#each");
+	if (!(numberOfPeople>1)){
+		console.log('Go each');
+		numberOfPeopleTag.value = 1;
+		numberOfPeople = 1;
+		each.style.display = 'none';
+	} else {
+		each.style.display = 'undefined'
+	}
+	let total = billAmount*(1+serviceQuality)/numberOfPeople;
+	total = total.toFixed(2);
+	totalTipTag.style.display='block'
+	TipTag.textContent = total;
+
+}
