5e58ed3c6d00b0d394a7acb9b7bab41d3cb01047
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-23 07:47:00 +0300

    exercises_xp_ninja_w08_d02_p2

---
+++ 08-week-08/day-02/exercise/exercises_xp_ninja_w08_d02_p2/script01.js
@@ -0,0 +1,13 @@
+//************* Exercise 1 : Menu */
+
+function mergeWords(string) {
+	return function(nextString) {
+		if (nextString === undefined) {
+			return string;
+		} else {
+			return mergeWords(string + ' ' + nextString);
+		}
+	}
+ }
+
+ console.log(mergeWords('There')('is')('no')('spoon.')());
